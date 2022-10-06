654e43c9d9f038d3b1a75ee086366fa17a3e6725
Jon Udell <judell@turbot.com>
2022-10-05 20:47:52 -0700

    add go.sum

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+
+# A Go client for the Hypothesis API
+
+[Hypothesis] is software for annotating the web. 
+
+This package implements [hypothesis.Client], a read-only interface to the [Hypothesis API].
+
+[Hypothesis]: https://web.hypothes.is
+[Hypothesis API]: https://h.readthedocs.io/en/latest/api/
+*/
+package hypothesis
