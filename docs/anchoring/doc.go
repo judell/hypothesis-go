594e179aa776ff3bac5409c6440441f5f3549dc4
Jon Udell <judell@turbot.com>
2022-10-09 13:28:38 -0700

    doclinks in doc-only package must be fully-specified

--- docs/anchoring/doc.go
+++ docs/anchoring/doc.go
@@ -6,6 +6,17 @@ When software creates annotations, it can define where they anchor in text using
 When software anchors annotations in text, it reads the TextQuoteSelector and/or TextPositionSelector structures from the API, and uses that info to modify the region of text they identify -- typically, by highlighting it and linking an action to it.
 
 When software analyzes an annotated corpus it can use the selector data to, e.g., visualize overlapping annotations. [hypothesis.SelectorsToExact] is a convenience function that returns the Exact quote from an array of selectors.
+
+[hypothesis.Client]: https://pkg.go.dev/github.com/judell/hypothesis-go#Client
+[hypothesis.Row]: https://pkg.go.dev/github.com/judell/hypothesis-go#Row
+[hypothesis.Selector]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector
+[hypothesis.Selector.Type]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector.Type
+[hypothesis.Selector.Start]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector.Start
+[hypothesis.Selector.End]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector.End
+[hypothesis.Selector.Prefix]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector.Prefix
+[hypothesis.Selector.Suffix]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector.Suffix
+[hypothesis.Selector.Exact]: https://pkg.go.dev/github.com/judell/hypothesis-go#Selector.Exact
+[hypothesis.SelectorsToExact]: https://pkg.go.dev/github.com/judell/hypothesis-go#SelectorsToExact
 */
 package anchoring
 
